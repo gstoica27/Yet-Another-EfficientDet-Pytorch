2d52f98805a137430286501af203357307091516
gstoica27 <gstoica27@gmail.com>
2020-07-28 23:51:50 -0700

    added detach + cpu + numpy conversion

--- resnet/embed_regions.py
+++ resnet/embed_regions.py
@@ -45,7 +45,7 @@ def embed_frame_regions(data, base_image, model):
     data['embeddings'] = []
     for roi in data['rois']:
         crop = crop_image(base_image, roi)
-        embedding = embed_region(model, crop).reshape(-1)
+        embedding = embed_region(model, crop).detach().cpu().numpy().reshape(-1)
         assert embedding.shape[0] == 2048, f"embedding shape is: {embedding.shape}"
         data['embeddings'].append(embedding)
     data['embeddings'] = np.array(data['embeddings'])
